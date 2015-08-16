37b303a61382c7d2c30c564791388fce7b76f165
adisnabawi <adisazizan@gmail.com>
2015-08-16 05:58:05 +0000

    Mailer

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -2,12 +2,12 @@ require "mandrill"
 
 class PostMailer < ActionMailer::Base
   def post_created(current_user)
-    mail(to: current_user.email, 
-    subject: "Thanks for your post", 
-    body: mandrill_template("foodhunt"))
     # mail(to: current_user.email, 
-    # from: "adisazizan@gmail.com",
-    # subject:"Thanks for your post",
-    # body:"Your restaurant has been posted. Thank you for using this application.")
+    # subject: "Thanks for your post", 
+    # body: mandrill_template("foodhunt"))
+    mail(to: current_user.email, 
+    from: "adisazizan@gmail.com",
+    subject:"Thanks for your post",
+    body:"Your restaurant has been posted. Thank you for using this application.")
   end
 end
