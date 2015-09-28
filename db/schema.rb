140bed37767feaa23d8d76573ed7f0b26fbd8695
adisnabawi <adisazizan@gmail.com>
2015-09-28 18:10:25 +0000

    Add Username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821165201) do
+ActiveRecord::Schema.define(version: 20150928170443) do
 
   create_table "comments", force: true do |t|
     t.string   "user_name"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20150821165201) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
